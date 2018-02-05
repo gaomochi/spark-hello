8ae093f035724f2e3e7d108d2bd3ea3341fbffe6
gaomochi <gaomochi@qq.com>
2018-02-05 17:52:29 +0800

    bugfix

--- src/main/scala/SparkSQLTest.scala
+++ src/main/scala/SparkSQLTest.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSQLTest {
   def main(args: Array[String]) {
-    val sparkSession = SparkSession.builder.master("yarn").appName("spark session example").getOrCreate()
+    val sparkSession = SparkSession.builder.getOrCreate()
     sparkSession.catalog.listDatabases().show(false)
     sparkSession.catalog.setCurrentDatabase("default")
     sparkSession.catalog.listTables().show(false)
