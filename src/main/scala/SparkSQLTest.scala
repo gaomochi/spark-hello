e5074e2e81ef7f35a15f25f99b031a6601bdad2f
gaomochi <gaomochi@qq.com>
2018-02-05 17:47:58 +0800

    bugfix

--- src/main/scala/SparkSQLTest.scala
+++ src/main/scala/SparkSQLTest.scala
@@ -3,11 +3,9 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSQLTest {
   def main(args: Array[String]) {
-    val sparkSession = SparkSession.builder.
-      master("yarn")
-      .appName("spark session example")
-      .getOrCreate()
+    val sparkSession = SparkSession.builder.master("yarn").appName("spark session example").getOrCreate()
     sparkSession.catalog.listDatabases().show(false)
+    sparkSession.catalog.setCurrentDatabase("default")
     sparkSession.catalog.listTables().show(false)
     sparkSession.sql("select * from core_users limit 10").show(false)
 
