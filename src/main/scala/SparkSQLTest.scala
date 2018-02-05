650de905daeef0e9fb5217db7c4ad3bbf3397a39
gaomochi <gaomochi@qq.com>
2018-02-05 18:52:29 +0800

    bugfix

--- src/main/scala/SparkSQLTest.scala
+++ src/main/scala/SparkSQLTest.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSQLTest {
   def main(args: Array[String]) {
-    val sparkSession = SparkSession.builder.getOrCreate()
+    val sparkSession = SparkSession.builder.enableHiveSupport().getOrCreate()
     sparkSession.catalog.listDatabases().show(false)
     sparkSession.catalog.setCurrentDatabase("default")
     sparkSession.catalog.listTables().show(false)
